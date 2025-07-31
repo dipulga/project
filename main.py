b873180561d39b722bb3e25e1c2b01a611b468eb
dipulga <dipulga@hotmail.com>
2025-07-30 21:25:02 -0500

    se adiciono nltk

---
+++ main.py
@@ -0,0 +1,9 @@
+# importar la libreria nltk
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# definir la ruta donde se almacenaran los datos de nltk
+nltk.data.path.append('C:\Users\dipul\AppData\Roaming\nltk_data')
+
